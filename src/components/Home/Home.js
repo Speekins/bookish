f067336e92eb9b03ce33c06bbae69bb4d2de878b
Spencer Haka <spencer.haka@gmail.com>
2023-01-11 21:38:05 -0700

    Complete CSS for banner and book covers

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import Book from '../Book/Book'
+import Banner from '../assets/images/books1.jpg'
+import './Home.css'
 
 const Home = ({ books }) => {
 
-  const allBooks = books.map((book, idx) => <Book name={book.name} rating={book.rating} year={book.year} key={idx} id={book.id}/>)
+  const allBooks = books.map((book, idx) => <Book name={book.name} cover={book.cover} rating={book.rating} year={book.year} key={idx} id={book.id} />)
 
   return (
     <>
-      <div className='banner'>This is the banner</div>
-      {allBooks}
+      <div className='banner'>
+        <img alt="Banner with books" className='banner-image' src={Banner} />
+        <h1 className='bookish'>BOOKISH</h1>
+      </div>
+      <div className='book-container'>
+        {allBooks}
+      </div>
     </>
   )
 }
