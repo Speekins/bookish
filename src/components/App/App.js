927b68c7d68052a34f690d800f42f041353007c0
Spencer Haka <spencer.haka@gmail.com>
2023-01-12 23:07:03 -0700

    Debug issue with useReducer re-setting state to initial state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useReducer } from 'react'
 import { Route, Routes } from 'react-router'
 import Home from '../Home/Home'
 import MyLibrary from '../MyLibrary/MyLibrary'
+import Book from '../Book/Book'
 import { getBooks } from '../../apiCalls'
 import { horror, fiction, nonFiction, history, memoir, scienceFiction, romance, mystery } from '../../production-data'
 
@@ -9,24 +10,21 @@ const initialState = {
   isLoading: true,
   books: {},
   myLibrary: [],
+  error: false,
+  data: null
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "SUCCESS":
-      return {
-        isLoading: false,
-        books: { ...state.books, [action.name]: action.payload },
-        error: false
-      }
-
+      console.log('success!', state)
+      return { ...state, isLoading: false, books: { ...state.books, [action.genre]: action.payload } }
+    case "FAVORITE":
+      console.log('favorite!')
+      return { ...state, isLoading: false, myLibrary: [...state.myLibrary, action.payload] }
     case "ERROR":
-      return {
-        isLoading: false,
-        data: null,
-        error: true
-      }
-
+      console.log('error!')
+      return { ...state, isLoading: false, error: true }
     default:
       return state
   }
@@ -34,34 +32,48 @@ const reducer = (state, action) => {
 
 const App = () => {
 
-  //const [books, setBooks] = useState(null)
   const [state, dispatch] = useReducer(reducer, initialState)
 
-
   useEffect(() => {
-    // const newBooks = async () => {
-    //   const yeah = await getBooks('https://hapi-books.p.rapidapi.com/week/mystery')
-    //   setBooks(yeah.slice(0, 6))
-    // }
-    // newBooks()
-    //setBooks(horror)
-    const genres = { horror: horror, fiction: fiction, nonFiction: nonFiction, history: history, memoir, scienceFiction: scienceFiction, romance: romance, mystery: mystery}
-    Object.values(genres).forEach((genre, idx) => {
-      let names = Object.keys(genres)
-      dispatch({ type: "SUCCESS", payload: genre, name: names[idx] })
+    const genres = { horror: horror, fiction: fiction, nonFiction: nonFiction, history: history, memoir, scienceFiction: scienceFiction, romance: romance, mystery: mystery }
+    Object.values(genres).forEach((booksByGenre, idx) => {
+      let genre = Object.keys(genres)[idx]
+      dispatch({ type: "SUCCESS", payload: formatBooks(booksByGenre, genre), genre: genre })
     })
-    
+
   }, [])
+
+  const formatBooks = (booksByGenre, genre) => {
+    let books = booksByGenre.map(book =>
+      <Book
+        name={book.name}
+        cover={book.cover}
+        url={book.url}
+        key={book.book_id}
+        book_id={book.book_id}
+        addToFavorites={addToFavorites}
+        isFavorite='false'
+        genre={genre}
+      />)
+    return books
+  }
+
+  const addToFavorites = (id, genre) => {
+    console.log(state)
+    // let book = state.books[genre].find(book => book.book_id === id)
+    // book.isFavorite = 'true'
+    // dispatch({ type: "FAVORITE", payload: book })
+  }
 
   return (
     <Routes>
       <Route
         exact path='/'
-        element={!state.isLoading && <Home books={state.books} />}
+        element={!state.isLoading && <Home books={state.books} addToFavorites={addToFavorites} />}
       />
       <Route
         path='my-library'
-        element={<MyLibrary />}
+        element={<MyLibrary myLibrary={state.myLibrary} />}
       />
     </Routes>
   )
--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -4,28 +4,38 @@ import './Book.css'
 import heart from '../assets/images/heart-regular.svg'
 import fillHeart from '../assets/images/heart-solid.svg'
 
-const Book = ({ name, cover }) => {
+const Book = ({ book_id, name, cover, url, isFavorite, addToFavorites, genre  }) => {
 
-  const [isFavorite, setIsFavorite] = useState(false)
+  //const [isFavorite, setIsFavorite] = useState(false)
 
   const favorite = <img src={fillHeart}
     alt="favorite icon"
     className='heart'
-    onClick={() => setIsFavorite(false)}
+    onClick={() => addToFavorites()}
   />
 
   const unFavorite = <img src={heart}
     alt="favorite icon"
     className='heart'
-    onClick={() => setIsFavorite(true)}
+    onClick={() => {
+      let book = <Book
+      name={name}
+      cover={cover}
+      url={url}
+      key={book_id}
+      book_id={book_id}
+      isFavorite='true'
+      addToFavorites={addToFavorites}
+    />
+      addToFavorites(book_id, genre)
+    }}
   />
 
   return (
     <div className='book'>
       <img alt={`Cover of ${name}`} src={`${cover}`} className="book-cover" />
       <div className='title'>
-        {!isFavorite && unFavorite}
-        {isFavorite && favorite}
+        {isFavorite ? unFavorite : favorite}
         <p className='name'>{name}</p>
       </div>
     </div>
