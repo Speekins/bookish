fa569a56352ba2c483062f79a9ec2f7a40f129e7
Spencer Haka <spencer.haka@gmail.com>
2023-04-05 21:02:08 -0600

    Start adding star rating system

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import NotesForm from '../NotesForm/NotesForm'
+import star from '../assets/images/star-regular.svg'
+import fillStar from '../assets/images/star-solid.svg'
 import './Modal.css'
 
 const Modal = ({ handleModalState, modalDetails, submitNotes }) => {
+
+  const [rating, setRating] = useState(0)
+
+  useEffect(() => {
+    document.getElementsByClassName('star').map(el => {
+      if (el.id <= rating) {
+        el.src = fillStar
+        return el
+      } else {
+        return el
+      }
+    })
+  }, [rating])
+
+  let starSet = <div className='star-set'>
+    <img src={star} alt="star" className='star' id='1' onClick={(event) => setRating(Number(event.target.id))} />
+    <img src={star} alt="star" className='star' id='2' onClick={(event) => setRating(Number(event.target.id))} />
+    <img src={star} alt="star" className='star' id='3' onClick={(event) => setRating(Number(event.target.id))} />
+    <img src={star} alt="star" className='star' id='4' onClick={(event) => setRating(Number(event.target.id))} />
+    <img src={star} alt="star" className='star' id='5' onClick={(event) => setRating(Number(event.target.id))} />
+  </div>
 
   return (
     <div className='modal-container'>
@@ -12,6 +35,7 @@ const Modal = ({ handleModalState, modalDetails, submitNotes }) => {
           <button className='close-modal' onClick={() => handleModalState()}>X</button>
         </span>
         <h1 className='modal-header'>{modalDetails.title}</h1>
+        {starSet}
         <div className='modal-details'>
           <img className='modal-cover' alt="Cover" src={modalDetails.book_image} />
           <div>
